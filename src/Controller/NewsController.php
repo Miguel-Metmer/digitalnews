3f2baf137263d70af4c6b800e5cc119d482c378b
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-01-21 18:09:02 +0100

    Ajout de fonctionnalités

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comments;
 
 class NewsController extends AbstractController
 {
@@ -14,4 +15,29 @@ class NewsController extends AbstractController
     {
         return $this->render('news/index.html.twig');
     }
+
+    /**
+     * @Route("/forum", name="forum")
+     */
+    public function forum()
+    {
+        $repo = $this->getDoctrine()->getRepository(Comments::class);
+        $comments = $repo->findAll();
+        return $this->render('news/forum.html.twig', [
+            "comments" => $comments
+        ]);
+    }
+
+    /**
+     * @Route("/forum/{id}", name="comment")
+     */
+    public function showForumComment($id)
+    {
+        $repo = $this->getDoctrine()->getRepository(Comments::class);
+        $comment = $repo->find($id);
+
+        return $this->render('news/comment.html.twig', [
+            "comment" => $comment
+        ]);
+    } 
 }
---
+++ src/Entity/Comments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): self
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
